c2f88b86d20d451ca9b837aa38092de41f1b3051
dev-dwr <dawid.rymarczyk02@gmail.com>
2023-04-08 17:48:40 +0200

    api url change

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     // API_URL: "http://localhost:8000", 
-    API_URL: "https://scripts-project-2023.herokuapp.com "
+    API_URL: "https://scripts-project-2023.herokuapp.com"
   }
 }
 
