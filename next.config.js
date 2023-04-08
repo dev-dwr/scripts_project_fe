401943b59bcdaf41ab74ff6ed3799f9cf49c4982
dev-dwr <dawid.rymarczyk02@gmail.com>
2023-04-08 14:58:54 +0200

    ready for deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    API_URL: "http://localhost:8000"
+    // API_URL: "http://localhost:8000", 
+    API_URL: "https://scripts-project-2023.herokuapp.com "
   }
 }
 
